518dbd20976c33e6519777d40abc163ed64a880b
Esteban <esteban.roa7@soy.sena.edu.co>
2023-09-22 22:12:17 -0500

    19 sep
    Ejercicios 19 sep

--- Javascript/Ejercicios 19 sep/script.js
+++ Javascript/Ejercicios 19 sep/script.js
@@ -7,32 +7,27 @@ if (edad >= 18) {
 }
 */
 
-/*2. Dada las tres calificaciones que un estudiante tuvo en su cuerpo:
+/*2. Dada las tres calificaciones que un estudiante tuvo en su curso:
 A. Determina su promedio.
 B. Indicar si el estudiante aprobó o no aprobó el curso.
 (Las notas deben ser de 1 a 10 y aprueba con un promedio mayor a 6).
 
-let claf = prompt ("Ingrese las calificacion:")
-let clf = prompt ("Ingrese la calificación:")
-let calf = prompt ("Ingrese la calificación:")
-let pro = 55/100
-let prom = 15/100
-let prm = 30/100
-let gan= parseInt(claf*pro)
-let ganal= parseInt(clf*prom)
-let gen= parseInt(calf*prm)
-let fin= parseInt(gan+ganal+gen)
+let claf1 = prompt ("Ingrese las calificacion:");
+let claf2 = prompt ("Ingrese la calificación:");
+let claf3 = prompt ("Ingrese la calificación:");
+let tclaf = parseInt (claf1)+ parseInt(claf2)+ parseInt(claf3);
+let prom = parseInt (tclaf/3);
+alert("Su clasificación es de: "+prom)
 
-alert("Su calificación de la materia es "+ fin + ".")
-
-if (fin >=7){  
-    alert("Aprobado"); 
-} else if(fin <7){  
-    alert(" Reprobado"); 
+if (prom>6){
+  alert("Su calificación es de: " +prom+ ". Por lo tanto, aprobó el curso.");
+}
+  else{
+    alert("Su calificación es de: " +prom+ ". Por lo tanto, NO aprobó el curso.");
 }
 */
 
-/* 3. Dado los números A y B, determinar si A es mayor, menor o igual que D.
+/* 3. Dado los números A y B, determinar si A es mayor, menor o igual que B.
 function compararNumeros(a, b) {
     if (a > b) {
       return "A es mayor que B.";
@@ -47,7 +42,7 @@ function compararNumeros(a, b) {
   let b = prompt("Ingrese el número B: ");
   let resultado = compararNumeros(a, b); 
   alert(resultado);
-  */ 
+*/
 
 /* 4. Que lea dos números y los imprima en forma ascendente.
 var A = prompt ("ingrese un numero:");
@@ -84,45 +79,47 @@ Día 1 lunes, Día 2 martes, Día 3 miércoles, Día 4 jueves, Día 5 viernes, D
 Día 7 domingo. Si se escribe otro número diferente debe salir el mensaje “ESCRIBIÓ UN NÚMERO
 FUERA DEL RANGO, ESCRIBA UN NÚMERO ENTRE 1 Y 7”.
 
-let dia = prompt("Ingrese un número entre 1 y 7: ");
-if (dia >= 1 && dia <= 7) {
-  switch (dia) {
-    case 1:
-      alert("Lunes");
+let dia = prompt("Ingrese un número entre 1 y 7:");
+dia = parseInt(dia);
+switch (dia) {
+    case 1: (dia = 1)
+      alert("Lunes.");
       break;
-    case 2:
-      alert("Martes");
+    case 2: (dia = 2)
+      alert("Martes.");
       break;
-    case 3:
-      alert("Miércoles");
+    case 3: (dia = 3)
+      alert("Miércoles.");
       break;
-    case 4:
-      alert("Jueves");
+    case 4: (dia = 4)
+      alert("Jueves.");
       break;
-    case 5:
-      alert("Viernes");
+    case 5: (dia = 5)
+      alert("Viernes.");
       break;
-    case 6:
-      alert("Sábado");
+    case 6: (dia = 6)
+      alert("Sábado.");
       break;
-    case 7:
-      alert("Domingo");
+    case 7: (dia = 7)
+      alert("Domingo.");
       break;
-  }
-} else {
-  alert("Escribió un número fuera del rango. Escriba un número entre 1 y 7");
+    
+    default:
+    alert("Escribió un número fuera del rango. Escriba un número entre 1 y 7.");
 }
 */
 
 /* 7. En un almacén se hace un 20% de descuento a los clientes cuya compra
 supere los $1000 pesos. A los demás solo se les hace un 5% de descuento ¿Cuál
 será la cantidad que pagará una persona por su compra?
+
 let cantidad_total = prompt("Ingrese el monto de la compra: ");
-if (cantidad_total >= 1000) {
-  let descuento = 0.2;
+cantidad_total = parseInt(cantidad_total);
+if (cantidad_total > 1000) {
+  let descuento = 0.2 * cantidad_total;
 } else {
-  let descuento = 0.05;
+  let descuento = 0.05 * cantidad_total;
 }
-let cantidad_a_pagar = cantidad_total - (cantidad_total * descuento);
-alert("La cantidad a pagar es de " + cantidad_a_pagar + " pesos.");
+let total = parseInt (cantidad_total) - parseInt (descuento);
+alert("La cantidad a pagar es de: $" + total + " pesos.");
 */
